d2879e5c547810daeb9f377dc6218173f5e9dfc5
tpessoa <tiagopessoa123@hotmail.com>
2023-03-31 22:55:28 +0100

    update bio and cv

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="bg-gradient-to-r from-[#C9D6FF] via-[#E2E2E2] to-[#C9D6FF] dark:from-[#141E30] dark:via-[#243B55] dark:to-[#141E30]">
+      <body className="bg-[#C9D6FF] dark:bg-[#141E30]">
         <Main />
         <NextScript />
       </body>
